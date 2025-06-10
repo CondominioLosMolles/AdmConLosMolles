03c77306c3f873a19eb6350c14b5013c12ed804b
CondominioLosMolles <losmollestunquen@gmail.com>
2025-06-10 15:13:56 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+window.CONFIG = {
+  SPREADSHEET_ID: "1bFo5dBC3HM0xupginTBe-hrrUNgkiuUn4fkXXzHide8",
+  CLIENT_ID: "997872453031-5o8s2o6v3qt722fb3p51a2r7bo24ncee.apps.googleusercontent.com",
+  API_KEY: "AIzaSyA4nUkMycf_CHZE7TaBBD_WUyWMvSXUwoU",
+  SCOPES: [
+    "https://www.googleapis.com/auth/spreadsheets", 
+    "https://www.googleapis.com/auth/drive.file", 
+    "https://www.googleapis.com/auth/gmail.send" 
+  ],
+  DEFAULT_TMC: 3.5,
+  ALLOWED_EMAIL: "losmollestunquen@gmail.com"
+};
