7b5627dea224bdf89cc60704b28a58e4847ec806
CondominioLosMolles <losmollestunquen@gmail.com>
2025-06-10 13:33:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // CONFIGURACIÓN DE CREDENCIALES Y API
 const CLIENT_ID = '997872453031-5o8s2o6v3qt722fb3p51a2r7bo24ncee.apps.googleusercontent.com';
-const API_KEY = 'TU_API_KEY_DE_GOOGLE_CLOUD'; // ¡¡¡REEMPLAZA ESTO!!!
+const API_KEY = 'AIzaSyA4nUkMycf_CHZE7TaBBD_WUyWMvSXUwoU'; 
 const SPREADSHEET_ID = '1bFo5dBC3HM0xupginTBe-hrrUNgkiuUn4fkXXzHide8';
 const SCOPES = 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/gmail.send';
 
