407c463bc0e4ebf02352e7ccad995e91c4824708
CondominioLosMolles <losmollestunquen@gmail.com>
2025-07-02 12:12:36 -0400

    Update sheets.js

--- js/sheets.js
+++ js/sheets.js
@@ -4,7 +4,7 @@
 const SPREADSHEET_ID = '1LwA_L8nfAh8TNhyb4xHjiAXetvik_eqxY6HX9jMNU0Y';
 
 // ID de tu Script
-const SCRIPT_ID = 'AKfycbzngLvhmMwTLjv4xRbeaqeF1Chl1PD01cCJQ_-xA9_985n8ExnyuN-lwxLGpfzHeD-m';
+const SCRIPT_ID = 'AKfycbyLkRi4RCDbyIFXiTKbh1WKM2nb841wVBC_i5ZKtgoCuSPs_ta8R_ndltOWyepxeC1Pyw';
 
 
 // --- Nombres de las Hojas ---
