6d629221f12952d60c5d13e7e100e58595f27bb5
CondominioLosMolles <losmollestunquen@gmail.com>
2025-07-09 09:58:11 -0400

    Delete .gitignore directory

--- .gitignore/config.js
+++
@@ -1,3 +0,0 @@
-// config.js
-// ¡IMPORTANTE! Este archivo NO se debe subir a GitHub.
-const OPENAI_API_KEY = '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'; // <-- Pega tu NUEVA clave aquí
