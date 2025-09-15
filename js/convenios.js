0590e1a28663fb3f446349ee33cd5f2f064f065b
CondominioLosMolles <losmollestunquen@gmail.com>
2025-09-15 09:42:12 -0300

    Update convenios.js

--- js/convenios.js
+++ js/convenios.js
@@ -575,7 +575,7 @@ const datosParaCrear = {
 };
 
 // 2. ¡IMPORTANTE! Reemplaza 'URL_DE_TU_SCRIPT' con la URL de implementación de tu Web App.
-const URL_DE_TU_SCRIPT = "https://script.google.com/macros/s/AKfycbzuN44ZQzJ2zGDNVSorn5pGlLy9V73ouUV5Df-eabqUpba8--ThYhzL-YyrNTS0eBEegg/exec"; // <--- ¡PEGA TU URL AQUÍ!
+const URL_DE_TU_SCRIPT = "https://script.google.com/macros/s/AKfycbzH5qHJaILNE2qaQQXUvZG1mi7NEMZZiRBFvv0pj4Zb8INtn219nMi-pU_VsJOB6gGTMg/exec"; // <--- ¡PEGA TU URL AQUÍ!
 
 // 3. Llamamos a la función `crearConvenio` en tu Google Apps Script.
 const response = await fetch(URL_DE_TU_SCRIPT, {
